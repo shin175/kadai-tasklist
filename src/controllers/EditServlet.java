5174a32a9f7e32e5dbb825955f8579c7ba8cf4ab
shinnosuke.ishida <shinnosuke.ishida0517@gmail.com>
2022-01-23 14:35:54 +0900

    EditServlet.javaを作成

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -2,11 +2,16 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class EditServlet
@@ -26,7 +31,20 @@ public class EditServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createEntityManager();
+
+        Task t = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("task", t);
+        request.setAttribute("_token", request.getSession().getId());
+
+        request.getSession().setAttribute("task_id", t.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+        rd.forward(request, response);
+
     }
 
 }

99facd25523df1e2d9f46185937d3ecf36633669
shinnosuke.ishida <shinnosuke.ishida0517@gmail.com>
2022-01-23 14:57:18 +0900

    DestroyServlet.javaを作成

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -2,11 +2,15 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class DestroyServlet
@@ -26,6 +30,21 @@ public class DestroyServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+            em.getTransaction().begin();
+            em.remove(t);
+            em.getTransaction().commit();
+            em.close();
+
+            request.getSession().removeAttribute("task_id");
+
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
     }
 
 }
